db5c253c582ca6d907d55e103f6e909629b9ddbb
rashmi <dr.rashmipatil@gmail.com>
2023-01-18 23:42:35 -0600

    added category code

--- src/main/java/org/launchcode/productpal/controllers/CategoryController.java
+++ src/main/java/org/launchcode/productpal/controllers/CategoryController.java
@@ -1,56 +1,75 @@
-package org.launchcode.productpal.controllers;
+package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.productpal.models.Category;
-import org.launchcode.productpal.models.data.CategoryRepository;
+import org.launchcode.productpal.models.CategoryService;
+import org.launchcode.productpal.models.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.validation.Valid;
-import java.util.Optional;
+import java.util.List;
 
 @Controller
-@RequestMapping("categories")
+@RequestMapping("category")
+
 public class CategoryController {
     @Autowired
-    private CategoryRepository categoryRepository;
-
-    @GetMapping("")
-    public String index (Model model){
-        model.addAttribute("title", "All Categories");
-        model.addAttribute("category", categoryRepository.findAll());
-        return "categories/index";
-    }
-
+    private CategoryService categoryService;
     @GetMapping("add")
-    public String displayAddCategoryForm(Model model) {
-        model.addAttribute("title", "Add Category");
-        model.addAttribute(new Category());
-        return "categories/add";
-    }
+    public String addCategory(Model model){
+        model.addAttribute("category",new Category());
+        model.addAttribute("pageTitle","Add category");
+        return "category/add";
 
+
+    }
+    @GetMapping("list")
+    public String listCategory(Model model){
+        List<Category> listCategory = categoryService.listCategory();
+        model.addAttribute("listCategory",listCategory);
+
+        return "category/list";
+    }
     @PostMapping("add")
-    public String processAddCategoryForm(@ModelAttribute @Valid Category newCategory,
-                                         Errors errors, Model model) {
-
-        if (errors.hasErrors()) {
-            return "categories/add";
-        }
-        categoryRepository.save(newCategory);
-        return "redirect:";
+    public String saveCategory(Category category){
+        System.out.println(category);
+        categoryService.save(category);
+        return "redirect:/category/list";
     }
+   /* @GetMapping("/category/edit/{id}")
+    public ModelAndView showEditCategoryPage(@PathVariable(name = "id") int id) {
+        ModelAndView mav = new ModelAndView("edit_category");
+       Category category =  categoryService.get(id);
+        mav.addObject("category", category);
 
-    @GetMapping("view/{categoryId}")
-    public String displayViewCategory(Model model, @PathVariable int categoryId) {
+        return mav;
+    }*/
+   @GetMapping("/edit/{id}")
+   public String showEditForm(@PathVariable("id")Integer id, Model model, RedirectAttributes ra){
+       try {
+           Category category=categoryService.get(id);
+           model.addAttribute("category", category);
+           model.addAttribute("pageTitle","Edit category");
+           return "category/add";
+       } catch (UserNotFoundException e) {
+           ra.addAttribute("message","category added successfully");
+           return "redirect:/category/list";
+       }
+   }
+    @GetMapping("/delete/{id}")
+    public String deleteCategory(@PathVariable("id")Integer id, Model model, RedirectAttributes ra){
+        try {
+            categoryService.deleteCategory(id);
+        } catch (UserNotFoundException e) {
+            ra.addAttribute("message","category added successfully");
 
-        Optional optCategory = categoryRepository.findById(categoryId);
-        if (optCategory.isPresent()) {
-            Category category = (Category) optCategory.get();
-            model.addAttribute("category", category);
-            return "categories/view";
         }
-        return "redirect:../";
+        return "redirect:/category/list";
     }
 }
---
+++ src/main/java/org/launchcode/productpal/models/CategoryService.java
@@ -0,0 +1,39 @@
+package org.launchcode.productpal.models;
+
+import org.launchcode.productpal.models.data.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+    public List<Category> listCategory(){
+        return (List<Category>) categoryRepository.findAll();
+
+    }
+
+    public void save(Category category) {
+        categoryRepository.save(category);
+    }
+
+     public Category get(Integer id) throws UserNotFoundException {
+            Optional<Category> result = categoryRepository.findById(id);
+            if(result.isPresent()){
+                return result.get();
+            }
+            throw new UserNotFoundException("Could not found user by ID");
+        }
+        public void deleteCategory(Integer id) throws UserNotFoundException {
+
+        Long count=categoryRepository.countById(id);
+        if(count == null|| count == 0){
+            throw new UserNotFoundException("Could not found user by ID");
+        }
+        categoryRepository.deleteById(id);
+        }
+    }
+
