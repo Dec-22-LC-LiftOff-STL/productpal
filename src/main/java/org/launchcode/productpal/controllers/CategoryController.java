eb4bc4816a715c5f1a6a0d70c691ea27279e0da9
kristo-baricevic <108290619+kristo-baricevic@users.noreply.github.com>
2023-01-23 00:03:54 -0500

    delete category works

--- src/main/java/org/launchcode/productpal/controllers/CategoryController.java
+++ src/main/java/org/launchcode/productpal/controllers/CategoryController.java
@@ -6,10 +6,7 @@ import org.launchcode.productpal.models.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -72,8 +69,8 @@ public class CategoryController {
         }
     }
 
-    @GetMapping("/delete/{id}")
-    public String deleteCategory(@PathVariable("id")Integer id, RedirectAttributes ra){
+    @PostMapping("/delete")
+    public String deleteCategory(@RequestParam("id")Integer id, RedirectAttributes ra){
         try {
             categoryService.deleteCategory(id);
             ra.addFlashAttribute("message", "Category deleted successfully");
